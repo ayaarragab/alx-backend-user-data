1f12739a82e2fa1f2cb26d43c5400b85067d3e7c
ayaarragab <ayarsk4343@gmail.com>
2024-08-31 20:46:43 +0300

    hash pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,21 +4,21 @@ a hash_password function that expects one
     string argument name password and returns
     a salted, hashed password, which is a byte string
 """
-from bcrypt import hashpw, gensalt
+import bcrypt
 import hashlib
 import base64
 from typing import ByteString
 
 
-def hash_password(password) -> ByteString:
+def hash_password(password: str) -> ByteString:
     """
     a hash_password function that expects one
     string argument name password and returns
     a salted, hashed password, which is a byte string
     """
     password = b"an incredibly long password" * 10
-    hashed = hashpw(
+    hashed = bcrypt.hashpw(
         base64.b64encode(hashlib.sha256(password).digest()),
-        gensalt()
+        bcrypt.gensalt()
     )
     return hashed
