f6a88660ba3231116ede5fb0cf2d5a0cc3fbc1ac
ayaarragab <ayarsk4343@gmail.com>
2024-08-31 20:51:40 +0300

    dcrypt pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,10 +5,20 @@ a hash_password function that expects one
     a salted, hashed password, which is a byte string
 """
 import bcrypt
-import hashlib
-import base64
 
 
 def hash_password(password: str) -> bytes:
     """ Returns byte string password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid function that expects 2 arguments and returns a boolean.
+
+    Arguments:
+
+    hashed_password: bytes type
+    password: string type
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
