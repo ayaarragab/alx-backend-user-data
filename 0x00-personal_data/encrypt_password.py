9aa1a6ae9e37c64dda3a724eaae5e911fc71226b
ayaarragab <ayarsk4343@gmail.com>
2024-08-31 20:48:15 +0300

    hash pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,18 +7,8 @@ a hash_password function that expects one
 import bcrypt
 import hashlib
 import base64
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
-    """
-    a hash_password function that expects one
-    string argument name password and returns
-    a salted, hashed password, which is a byte string
-    """
-    password: str = b"an incredibly long password" * 10
-    hashed: ByteString = bcrypt.hashpw(
-        base64.b64encode(hashlib.sha256(password).digest()),
-        bcrypt.gensalt()
-    )
-    return hashed
+def hash_password(password: str) -> bytes:
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
