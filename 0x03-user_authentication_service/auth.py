7680d7ed5163e1306d34c426171a355118ca7770
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 21:20:03 +0300

    session_if

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,6 +43,18 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        creates session_id of a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pass
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+        
 
 def _hash_password(password: str) -> bytes:
     """ Returns byte string password """
