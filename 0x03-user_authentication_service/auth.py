23896b95ebe75c2fda8d17a07e5ffc0e7d4b34be
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 21:14:22 +0300

    uuid4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,7 +49,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """
         The function should return a
         string representation of a new UUID.
