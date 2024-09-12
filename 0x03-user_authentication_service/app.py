5679338622ff1a01681e85c40265be6438194543
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 20:46:46 +0300

    1st app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,10 +7,11 @@ from flask import Flask, jsonify
 app = Flask()
 
 
-@app.route("/")
-def hello():
-    return jsonify({"message": "Bienvenue"})
-
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """ Base route for authentication service API """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
