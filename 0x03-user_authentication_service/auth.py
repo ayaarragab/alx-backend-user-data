313250b66e75f5a80fa90d23e4bf6b11c42021c5
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 20:37:42 +0300

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,31 @@ a hash_password function that expects one
     a salted, hashed password, which is a byte string
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
+from user import User
+from uuid import uuid4
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registering user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashedpass = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hashedpass)
+        else:
+            raise ValueError(f'User {email} already exists')
 
 
 def _hash_password(password: str) -> bytes:
