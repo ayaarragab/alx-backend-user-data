22535f9e209838e21e062cc79bf24dca2d9f5779
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 18:30:03 +0300

    adds a user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" holds class User"""
-from sqlalchemy import Column, String, Integer
+"""Class User for ORM"""
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
@@ -11,5 +12,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
