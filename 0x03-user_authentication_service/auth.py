e8ec007824b1234aa6056e524d91c0a742df74c0
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 20:25:48 +0300

    hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,6 @@ a hash_password function that expects one
 import bcrypt
 
 
-def hash_password(password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """ Returns byte string password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
