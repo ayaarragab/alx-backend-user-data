c5d4c2f1f370ee92720b810bb0b8076b544075e9
ayaarragab <ayarsk4343@gmail.com>
2024-09-12 21:41:40 +0300

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,6 +67,11 @@ class Auth:
             return user
         except NoResultFound:
             return None
+    def destroy_session(self, user_id: int) -> None:
+        """
+            destroys session of a user
+        """
+        self._db.update_user(user_id, session_id=None)
 
 
 def _hash_password(password: str) -> bytes:
